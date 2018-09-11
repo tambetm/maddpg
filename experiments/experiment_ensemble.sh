394c0f0ee9f3d22a36fd67e4a114e15ebf1adc05
Tambet Matiisen <tambet.matiisen@gmail.com>
2018-09-11 16:17:37 +0300

    evert "Merge."
    This reverts commit f4f09829a65be6c38c52404c8b341d7f420eb7d3.

--- experiments/experiment_ensemble.sh
+++ experiments/experiment_ensemble.sh
@@ -5,13 +5,13 @@ shift
 
 mkdir -p policies figures benchmark_files learning_curves
 
-#python ensemble.py --exp-name ${label} --save-dir policies/${label}/policy $*
-#python learning_curve.py learning_curves/${label}_rewards.pkl figures/${label}_learning_curve.png
-#python ensemble.py --exp-name ${label}_eval --load-dir policies/${label}/policy --restore --benchmark --save-replay --deterministic $*
-#python statistics.py benchmark_files/${label}_eval.pkl >> ${label%_*}.csv
-#python prepare_ensemble.py benchmark_files/${label}_eval.pkl --policy_file policies/${label}/policy --output_file benchmark_files/${label}_eval.npz $*
-#python figure.py benchmark_files/${label}_eval.npz figures/${label}.png
-#python prepare_ensemble.py benchmark_files/${label}_eval.pkl $*
+python ensemble.py --exp-name ${label} --save-dir policies/${label}/policy $*
+python learning_curve.py learning_curves/${label}_rewards.pkl figures/${label}_learning_curve.png
+python ensemble.py --exp-name ${label}_eval --load-dir policies/${label}/policy --restore --benchmark --save-replay --deterministic $* --ensemble-choice episode
+python statistics.py benchmark_files/${label}_eval.pkl >> ${label%_*}.csv
+python prepare_ensemble.py benchmark_files/${label}_eval.pkl --policy_file policies/${label}/policy --output_file benchmark_files/${label}_eval.npz $*
+python accuracy.py benchmark_files/${label}_eval.npz figures/${label}_accuracy.npy
+python figure.py figures/${label}_accuracy.npy figures/${label}.png
 
 python sheldon_ensemble.py --exp-name ${label}_sheldon12_landmark12 --num-sheldons 2 --sheldon-ids 1 2 --sheldon-targets 1 2  --load-dir policies/${label}/policy --restore --benchmark --save-replay --deterministic $*
 python sheldon_ensemble.py --exp-name ${label}_sheldon12_landmark02 --num-sheldons 2 --sheldon-ids 1 2 --sheldon-targets 0 2  --load-dir policies/${label}/policy --restore --benchmark --save-replay --deterministic $*
